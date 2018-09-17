12db0a981589d6d07d13ac6d30c9e98d5895b5aa
gondaimgano <gondaimgano@gmail.com>
2018-09-17 23:17:37 +0200

    Making changes to the server address which were provided in the technical specs. Also made changes to the number of items retrieved from 10 to 20 and also changing the min sdk to 16 instead of 23

--- app/src/main/java/get/gondai/demo/util/DeliveryUtil.kt
+++ app/src/main/java/get/gondai/demo/util/DeliveryUtil.kt
@@ -1,8 +1,8 @@
 package get.gondai.demo.util
 
 object DeliveryUtil {
-       val OFFSET=10
-       val LIMIT=10
+       val OFFSET=20
+       val LIMIT=20
        val NETWORK_ERROR=500
        val DELAY: Long=3
        val NETWORK_SIGNAL_DOWN: Int=300
